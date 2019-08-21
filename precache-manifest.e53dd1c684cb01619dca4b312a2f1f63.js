b7b69e82e5e23efeb0c11baac73cb449fe391512
Charles Campbell <44929571+C-E-Campbell@users.noreply.github.com>
2019-08-21 10:15:50 -0400

    Add files via upload

---
+++ precache-manifest.e53dd1c684cb01619dca4b312a2f1f63.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "593820e13b2435b5e821f02ba691dd65",
+    "url": "/monster-rolodex/index.html"
+  },
+  {
+    "revision": "892b6811aca94cd1e7bc",
+    "url": "/monster-rolodex/static/css/main.d5e20f33.chunk.css"
+  },
+  {
+    "revision": "9146193c4635cb5cf320",
+    "url": "/monster-rolodex/static/js/2.2ef0ace2.chunk.js"
+  },
+  {
+    "revision": "892b6811aca94cd1e7bc",
+    "url": "/monster-rolodex/static/js/main.25f499e6.chunk.js"
+  },
+  {
+    "revision": "f76bcdaeec9a3a77c2b6",
+    "url": "/monster-rolodex/static/js/runtime~main.ca553a7c.js"
+  }
+]);
